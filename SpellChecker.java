7709e1b652ef1c87e8cf3ca1a060843141551c0a
Sumedha <36409967+thechume@users.noreply.github.com>
2018-04-28 16:33:36 +0530

    Add files via upload
    I added rest of the rules but it gives errors. Added from rule 6 to 13

--- SpellChecker.java
+++ SpellChecker.java
@@ -18,21 +18,197 @@ import java.io.*;
 				tempWord=tempWord.substring(0,tempWord.length()-1);
 			tempWord=transformC(tempWord);
 			tempWord=transformD(tempWord);
-			tempWord=reduceVowels(tempWord); //this has to be last step. do all other reductions based on rule before this.
+			
+			tempWord=transformG(tempWord);
+			tempWord=transformH(tempWord);
+			tempWord=transform9_10(tempWord);
+			tempWord=transformQ(tempWord);
+			tempWord=transformS(tempWord);
+			tempWord=transformT(tempWord);
+			tempWord=transformV(tempWord);
+			tempWord=transformX(tempWord);
+			tempWord=transformY(tempWord);
+			tempWord=transformZ(tempWord);
+			
+			tempWord=reduceVowels(tempWord); 
 			return tempWord;
+		}
+		
+		//method to simplify Z. rule 18
+		static String transformZ(String word)
+		{
+			for(int curi=0;curi<word.length();curi++)
+			{
+				if(word.substring(curi,curi+1)=="Z")
+					word=word.substring(0,curi)+"S"+word.substring(curi+1);
+			}
+			return word;
+		}
+		
+		//method to simplify Y. rule 17
+		static String transformY(String word)
+		{
+			for(int curi=0;curi<word.length()-2;curi++)
+			{
+				if((word.substring(curi,curi+2)!="YA")||(word.substring(curi,curi+2)!="YE")||(word.substring(curi,curi+2)!="YI")||(word.substring(curi,curi+2)!="YO")||(word.substring(curi,curi+2)!="YU"))
+					word=word.substring(0,curi)+word.substring(curi+1);
+			}
+			return word;
+		}
+		
+		//method to simplify X. rule 16
+		static String transformX(String word)
+		{
+			if(word.substring(0,1)=="X")
+				word="S"+word.substring(2);
+			for(int curi=1;curi<word.length()-1;curi++)
+				if(word.substring(curi,curi+1)=="X")
+					word=word.substring(0,curi)+"KS"+word.substring(curi+1);
+			return word;
+		}
+		
+		//method to simplify W. rule 15
+		static String transformW(String word)
+		{
+			if(word.substring(0,2)=="WH")
+				word="W"+word.substring(2);
+			for(int curi=0;curi<word.length()-2;curi++)
+			{
+				if((word.substring(curi,curi+2)!="WA")||(word.substring(curi,curi+2)!="WE")||(word.substring(curi,curi+2)!="WI")||(word.substring(curi,curi+2)!="WO")||(word.substring(curi,curi+2)!="WU"))
+					word=word.substring(0,curi)+word.substring(curi+1);
+			}
+			return word;
+		}
+		
+		//method to simplify V. rule 14
+		static String transformV(String word)
+		{
+			for(int curi=0;curi<word.length()-1;curi++)
+			{
+				if(word.substring(curi,curi+1)=="V")
+					word=word.substring(0,curi)+"F"+word.substring(curi+1);
+			}
+			return word;
+		}
+		
+		//method to simplify S. rule 12
+		static String transformS(String word)
+		{
+			for(int curi=0;curi<word.length()-2;curi++)
+			{
+				if((word.substring(curi,curi+2)=="SH")||(word.substring(curi,curi+3)=="SIO")||(word.substring(curi,curi+3)=="SIA"))
+					word=word.substring(0,curi)+"X"+word.substring(curi+1);
+			}
+			return word;
+		}
+		
+		//method to simplify T. rule 13
+				static String transformT(String word)
+				{
+					for(int curi=0;curi<word.length()-3;curi++)
+					{
+						if((word.substring(curi,curi+3)=="TIO")||(word.substring(curi,curi+3)=="TIA"))
+							word=word.substring(0,curi)+"X"+word.substring(curi+1);
+						if((word.substring(curi,curi+3)=="TCH"))
+							word=word.substring(0,curi)+word.substring(curi+1);
+					}
+					for(int curi=0;curi<word.length()-2;curi++)
+						if((word.substring(curi,curi+2)=="TH"))
+							word=word.substring(0,curi)+word.substring(curi+1);
+					return word;
+				}
+		
+		//method to simplify Q. rule 11
+		static String transformQ(String word)
+		{
+			for(int curi=0;curi<word.length()-1;curi++)
+			{
+				if(word.substring(curi,curi+1)=="Q")
+					word=word.substring(0,curi)+"K"+word.substring(curi+1);
+			}
+			return word;
+		}
+		
+		//method to simplify CK and PH (rules 9 and 10)
+		static String transform9_10(String word)
+		{
+			for(int curi=0;curi<word.length()-2;curi++)
+			{
+				if(word.substring(curi,curi+2)=="CK")
+					word=word.substring(0,curi)+word.substring(curi+1);
+				if(word.substring(curi,curi+2)=="PH")
+					word=word.substring(0,curi)+"F"+word.substring(curi+2);
+			}
+			return word;
+		}
+		
+		//method to simply H. rule 8
+		static String transformH(String word)
+		{
+			for(int curi=0;curi<word.length()-2;curi++)
+			{
+				if(word.substring(curi,curi+2)=="AH"||word.substring(curi,curi+2)=="IH"||word.substring(curi,curi+2)=="EH"||word.substring(curi,curi+2)=="OH"||word.substring(curi,curi+2)=="UH")
+					if(word.substring(curi+2,curi+3)!="A"||word.substring(curi+2,curi+3)!="E"||word.substring(curi+2,curi+3)!="I"||word.substring(curi+2,curi+3)!="O"||word.substring(curi+2,curi+3)!="U")
+					{
+						word=word.substring(0,curi)+word.substring(curi+1);
+					}
+			}
+			return word;
+		}
+		
+		//method to simplify G. rules 6 and 7
+		static String transformG(String word)
+		{
+			String tempWord="";
+			
+			//for rule 6
+			if((word.substring(word.length()-2)=="GN")||(word.substring(word.length()-2)=="GH"))
+				tempWord=word.substring(0,word.length()-3)+word.substring(word.length()-1);
+			
+			if(word.length()>5)
+				if(word.substring(word.length()-5).equals("GNED"))
+					tempWord=word.substring(0,word.length()-5)+word.substring(word.length()-3);
+			
+			if(tempWord!="")
+				word=tempWord;
+			
+			for(int curi=0;curi<word.length()-2;curi++)
+			{
+				if(word.substring(curi,curi+2).equals("GH"))
+					 if(word.substring(curi+2,curi+2)=="A"||word.substring(curi+2,curi+2)=="E"||word.substring(curi+2,curi+2)=="I"||word.substring(curi+2,curi+2)=="O"||word.substring(curi+2,curi+2)=="U")
+					 {
+						 tempWord=word.substring(0,curi)+word.substring(curi+1);
+						 word=tempWord;
+					 }
+			}
+				
+			//for rule 7
+			for(int curi=0;curi<word.length()-2;curi++)
+			{
+				if(word.substring(curi,curi+2)=="GI"||word.substring(curi,curi+2)=="GE"||word.substring(curi,curi+2)=="GY")
+					if(word.substring(curi-1,curi+1)!="GG")
+						tempWord=word.substring(0,curi)+"J"+word.substring(curi+1);
+				else
+					tempWord=word.substring(0,curi)+"K"+word.substring(curi+1);	
+			}
+			if(tempWord.equals(""))
+				return word;
+			else
+				return tempWord;
 		}
 		
 		
 		//method to simplify D. rule number 5
 		static String transformD(String word) {
 			String tempWord="";
-			for(int curi=0;curi<word.length();curi++)
+			for(int curi=0;curi<word.length()-1;curi++)
 			{
-				if(word.substring(curi,curi+1).equals("d"))
+				if(word.substring(curi,curi+1).equals("D"))
 				{
-					if(curi+3<=word.length()) {
-					if(word.substring(curi+1,curi+3).equals("GE")||word.substring(curi+1,curi+3).equals("GY")||word.substring(curi+1,curi+3).equals("GI"))
-						tempWord=word.substring(0,curi)+"J"+word.substring(curi+1);
+					if(curi+3<=word.length()) 
+					{
+						if(word.substring(curi+1,curi+3).equals("GE")||word.substring(curi+1,curi+3).equals("GY")||word.substring(curi+1,curi+3).equals("GI"))
+							tempWord=word.substring(0,curi)+"J"+word.substring(curi+1);
 					}
 					else
 						tempWord=word.substring(0,curi)+"T"+word.substring(curi+1);
@@ -116,10 +292,10 @@ import java.io.*;
 	  	static int c; //to count number of suggestions printed
 	    static String inputWord;
 	    static String newMetaphone;
-    	static String dictWord=new String(); //to store dictionary word
-    	static String dictMet=new String(); //to store metaphone of the dictionary word
-    	static String suggestions[]=new String[30];
-    	static int order[]=new int[30];
+    	static String dictWord; //to store dictionary word
+    	static String dictMet; //to store metaphone of the dictionary word
+    	static String suggestions[]=new String[100];
+    	static int order[]=new int[100];
     	
     	
 	     public static void main(String []args){
@@ -152,7 +328,7 @@ import java.io.*;
 	     {
 	        Scanner sc=new Scanner(System.in);
 	        System.out.println("Enter word :");
-	        inputWord=sc.next();
+	        inputWord=sc.nextLine();
 	        inputWord=inputWord.toLowerCase();
 	        //long t1=System.nanoTime();
 	        int flag=0;
@@ -185,9 +361,8 @@ import java.io.*;
 	        		dictWord=itr.next();
 	        		dictMet=find_Metaphones(dictWord);
 	        		int d=dist.editDistDP(newMetaphone,dictMet,newMetaphone.length(),dictMet.length());
-	        		int d=dist.editDistDP(newMetaphone,dictMet,newMetaphone.length(),dictMet.length());
 	        			//System.out.println(newMetaphone+" "+dictSets[0]+" "+dictSets[1]+" "+d);
-	        			if(d<4)
+	        			if(d<2)
 	        			{	
 	        				suggestions[c]=dictWord;
 	        				order[c]=d;
@@ -223,25 +398,12 @@ import java.io.*;
 	        	
 	        	for(int i=0;i<4;i++)
 	        	{
-	        		System.out.println(suggestions[i]);
+	        		if(suggestions[i]!=null)
+	        			System.out.println(suggestions[i]);
 	        	}
 	        	
-	        	/*int count=0;
-	        	for(int i=0;i<4;i++)
-	        	{
-	        		for(int j=0;j<c;j++)
-	        		{	if(order[j]==i)
-	        			{
-	        				System.out.println(suggestions[j]);
-	        				count++;
-	        			}
-	        		if(count==4)
-	        			break;
-	        		}
-	        		if(count==4)
-	        			break;
-	        	}*/
 	        }
 	        	
 	     }     		
+	     
 	}
